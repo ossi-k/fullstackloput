5610ec666b3089b60ff2d8b074130a2ffd3f10d6
ossi-k <ossi.kohvakka@gmail.com>
2020-10-01 18:36:18 +0300

    Osa 4: tehty tehtävä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,24 @@
 const dummy = (blogs) => {
     return 1
+}
+
+/* const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    return blogs.reduce(reducer, 0)
+} */
+
+const totalLikes = array => {
+    const reducer = (sum, item) => {
+      return sum + item.likes
+    }
+    return array.length === 0
+      ? 0 
+      : array.reduce(reducer, 0)
   }
-  
-  module.exports = {
+
+module.exports = {
+    totalLikes,
     dummy
-  }
+}
