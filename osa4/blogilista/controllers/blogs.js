637b2754ea855e1d4a2324e6e84e62c32dfc2610
ossi-k <ossi.kohvakka@gmail.com>
2020-10-12 21:40:41 +0300

    Osa 4: Tehty tehtävä 4.17 (tosin testit failaavat nyt, koska userid pitäisi saada sinne pyörimään jotenkin...)

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,15 +1,24 @@
 const blogsRouter = require('express').Router()
-const { response } = require('../app')
+//const { response } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({}).populate('user', {username : 1, name : 1, id : 1})
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
 
     const body = request.body
+
+    console.log('body on: ' + body)
+    console.log('bodyn title: ' + body.title)
+    console.log('bodyn user: ' + body.user)
+    console.log('bodyn userid: ' + body.userid)
+
+    const user = await User.findById(body.userid)
 
     if (body.likes === '') {
         body.likes = '0'
@@ -19,19 +28,26 @@ blogsRouter.post('/', async (request, response) => {
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes,
+        user: user._id
     })
 
     if (blog.title === '' || blog.url === '') {
         response.status(400).end()
     } else {
         const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
         response.json(savedBlog.toJSON)
     }
-    
+
+    /*const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog,_id)
+    response.json(savedBlog.toJSON)*/
+
 })
 
-blogsRouter.delete('/:id', async(request, response) => {
+blogsRouter.delete('/:id', async (request, response) => {
     await Blog.findByIdAndDelete(request.params.id)
     response.status(204).end()
 })
