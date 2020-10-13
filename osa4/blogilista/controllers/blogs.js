c920576a56d6e2b82adf4ee6c47f5bb55d078e27
ossi-k <ossi.kohvakka@gmail.com>
2020-10-13 16:38:29 +0300

    Osa 4: Tehty tehtävä 4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,6 +2,7 @@ const blogsRouter = require('express').Router()
 //const { response } = require('../app')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -9,16 +10,33 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
+console.log('tapahtuuko mitään?')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    console.log('authorization ' + authorization)
+    if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
 
     const body = request.body
+    const token = getTokenFrom(request)
+    console.log("token: " + token)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid'})
+    }
 
     console.log('body on: ' + body)
     console.log('bodyn title: ' + body.title)
     console.log('bodyn user: ' + body.user)
     console.log('bodyn userid: ' + body.userid)
 
-    const user = await User.findById(body.userid)
+    const user = await User.findById(decodedToken.id)
 
     if (body.likes === '') {
         body.likes = '0'
