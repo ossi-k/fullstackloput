f3a274ebda7ca9e28997003d2f3350525b71061d
ossi-k <ossi.kohvakka@gmail.com>
2020-10-13 18:28:41 +0300

    Osa 5: tehty tehtävä 5.2

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -17,12 +17,26 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJson = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJson) {
+      const user = JSON.parse(loggedUserJson)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -35,9 +49,17 @@ const App = () => {
     //console.log('logging in with', username, password)
   }
 
+  const handleLogut = async (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBlogappUser')
+    window.localStorage.clear()
+    window.location.reload(true)
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
+        <h2>Login</h2>
         username
           <input
           type="text"
@@ -59,6 +81,12 @@ const App = () => {
     </form>
   )
 
+/*   const logoutButton = () => (
+    <div>
+      <button onClick={handleLogut}>logout</button>
+    </div>
+  )
+ */
   /*   const noteForm = () => {
       <form onSubmit={addBlog}>
         <input
@@ -74,15 +102,20 @@ const App = () => {
     <div>
       <h2>blogs</h2>
 
-      <h2>Login</h2>
+
 
       {user === null ?
         loginForm() :
+
         <div>
           <p>{user.name} logged in</p>
+          <div>
+            <button onClick={handleLogut}>logout</button>
+          </div>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
-      )}
+          )}
+
         </div>
       }
 
