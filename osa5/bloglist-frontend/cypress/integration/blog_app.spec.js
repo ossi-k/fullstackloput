ff0afd93bec32b0117db56107ef5b53d749ab914
ossi-k <ossi.kohvakka@gmail.com>
2020-10-16 11:08:14 +0300

    osa 5: tehty tehtävä 5.17

---
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -0,0 +1,7 @@
+describe('Blog ', function() {
+    it('front page can be opened', function() {
+      cy.visit('http://localhost:3000')
+      cy.contains('blogs')
+      cy.contains('Login')
+    })
+  })
