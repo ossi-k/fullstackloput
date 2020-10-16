9f40b3d0517212011830469caebe67726200f0ca
ossi-k <ossi.kohvakka@gmail.com>
2020-10-16 15:10:25 +0300

    Osa 5: tehty tehtävä 5.18

--- osa5/bloglist-frontend/cypress/integration/blog_app_front_page.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app_front_page.spec.js
@@ -1,13 +1,38 @@
 describe('Blog app', function() {
-    beforeEach(function() {
-      cy.request('POST', 'http://localhost:3001/api/testing/reset')
-      cy.visit('http://localhost:3000')
-    })
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Testaaja',
+      username: 'testaaja',
+      password: 'salasana'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
   
     it('Login from is shown', function() {
         cy.contains('blogs')
         cy.contains('Login')
         cy.contains('username')
         cy.contains('password')
+        //cy.contains('login').click()
     })
-  })
+
+   describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testaaja')
+      cy.get('#password').type('salasana')
+      cy.get('#login-button').click()
+
+      cy.contains('Testaaja logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('väärä')
+      cy.get('#password').type('salasana')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong credentials')
+    })
+  }) 
+})
